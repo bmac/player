a23c6d0df823e057fad3d2b2fb15436c4a8c066f
bmac <bmac325@gmail.com>
2012-12-15 13:42:26 -0500

    Added play pause and range seek event handlers.

--- app/player.js
+++ app/player.js
@@ -17,41 +17,64 @@ function($, _, Backbone) {
 	    ended: 'showPlayButton',
 	    pause: 'showPlayButton',
 	    play: 'showPauseButton',
-	    'timeupdate audio': 'onTimeupdate'
+	    'click .play': 'playAudio',
+	    'click .pause': 'pauseAudio',
+	    'timeupdate audio': 'onTimeupdate',
+	    'change .player-range': 'rangeSeek'
 //	    volumechange: ''
 	},
 	initialize: function() {
 	    this._propagateAudioEvents();
-	    window.audio = this.$('audio')[0];
+	    window.audio = this.audio = this.$('audio')[0];
+	    this.$range = this.$('.player-range').val(0);
+	    this.$time = this.$('.player-timestamp');
 	},
 
 	onTimeupdate: function() {
-	    console.log('time update');
-	    var audio = this.$('.player-audio')[0];
-	    var percent = Math.floor((audio.currentTime / audio.duration) * 100) || 0;
-	    var range = this.$('.player-range');
-	    range.prop('value', percent);
+	    var percent = Math.floor((this.audio.currentTime / this.audio.duration) * 100) || 0;
+	    this.$range.val(percent);
+	    this.$time.val(this._formatTime(this.audio.currentTime));
 	},
 
 	showPlayButton: function() {
-	    this.$('.play').text('|>');
+	    this.$('.pause').text('|>').removeClass('pause').addClass('play');
 	},
 	showPauseButton: function() {
-	    this.$('.play').text('||');
+	    this.$('.play').text('||').removeClass('play').addClass('pause');
+	},
+	playAudio: function() {
+	    this.audio.play();
+	},
+	pauseAudio: function() {
+	    this.audio.pause();
+	},
+	rangeSeek: function() {
+	    var percent = this.$range.val();
+	    var newCurrentTime = this.audio.duration * (percent / 100);
+	    this.audio.currentTime = newCurrentTime;
 	},
 	_propagateAudioEvents: function() {
 	    var audioEvents = 'timeupdate play pause ended'.split(' ');
 	    var $audio = this.$el.find('audio');
-	    _.each(audioEvents, function(event) {
-		$audio.on(event, function (e) {
+	    this._propagateEvents($audio, audioEvents);
+	},
+	_propagateEvents: function(element, events) {
+	    var $element = element;
+	    _.each(events, function(event) {
+		$element.on(event, function (e) {
 		    if (!e.isTrigger) {
 			$.event.simulate( event, e.target, $.event.fix( e ), true );
 		    }
 		});
 	    });
 
+	},
+	_formatTime: function(rawSeconds) {
+	    var hours = parseInt( rawSeconds / 3600 ) % 24,
+	    minutes = parseInt( rawSeconds / 60 ) % 60,
+	    seconds = parseInt( rawSeconds )  % 60;
+	    return [minutes, ':', (seconds  < 10 ? "0" + seconds : seconds)].join('');
 	}
-
 
 	});
 
