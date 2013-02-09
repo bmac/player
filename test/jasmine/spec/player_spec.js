743be50caf8743545e296a5b45b348025b410dcd
bmac <bmac325@gmail.com>
2013-02-09 15:34:50 -0500

    add a test for the volumne slider

--- test/jasmine/spec/player_spec.js
+++ test/jasmine/spec/player_spec.js
@@ -1,56 +1,63 @@
 define(['player'], function(player) {
-  describe("PlayerView", function() {
-    var playerView;
-    var reset_audio_element = function(playerView) {
-        playerView.audio.currentTime = 0;
-        playerView.$duration.val('0');
-        playerView.$time.val('');
-    };
-    beforeEach(function() {
-      playerView = new player.PlayerView();
-      reset_audio_element(playerView);
-    });
+    describe("PlayerView", function() {
+        var playerView;
+        var reset_audio_element = function(playerView) {
+            playerView.audio.currentTime = 0;
+            playerView.$duration.val('0');
+            playerView.$time.val('');
+        };
+        beforeEach(function() {
+            playerView = new player.PlayerView();
+            reset_audio_element(playerView);
+        });
 
-    afterEach(function() {
-    // console.log("After");
-    });
+        afterEach(function() {
+            // console.log("After");
+        });
 
-    it("should initialize some properties on the view object", function() {
-      expect(playerView.$duration).toBeTruthy();
-      expect(playerView.$time).toBeTruthy();
-      expect(playerView.$volumeRange).toBeTruthy();
-    });
+        it("should initialize some properties on the view object", function() {
+            expect(playerView.$duration).toBeTruthy();
+            expect(playerView.$time).toBeTruthy();
+            expect(playerView.$volumeRange).toBeTruthy();
+        });
 
-    it("should have a play button", function() {
-      expect(playerView.$('.play').text()).toBe('|>');
-    });
+        it("should have a play button", function() {
+            expect(playerView.$('.play').text()).toBe('|>');
+        });
 
-    describe("when a timeupdate event is triggered", function() {
-      beforeEach(function() {
-        playerView = new player.PlayerView();
-	reset_audio_element(playerView);
-      });
+        describe("when a timeupdate event is triggered", function() {
+            beforeEach(function() {
+                playerView = new player.PlayerView();
+                reset_audio_element(playerView);
+            });
 
-      it("should update the position of the range slider", function() {
-        var audio = playerView.audio;
-        audio.currentTime = audio.duration / 2;
-        expect(playerView.$duration.val()).toBe('0');
-        $(audio).trigger('timeupdate');
-        expect(playerView.$duration.val()).toBe('50');
-      });
+            it("should update the position of the range slider", function() {
+                var audio = playerView.audio;
+                audio.currentTime = audio.duration / 2;
+                expect(playerView.$duration.val()).toBe('0');
+                $(audio).trigger('timeupdate');
+                expect(playerView.$duration.val()).toBe('50');
+            });
 
-      it("should update value of the time input", function() {
-        var audio = playerView.audio;
-	audio.currentTime = 0;
-        $(audio).trigger('timeupdate');
-        expect(playerView.$time.val()).toBe('0:00');
-        audio.currentTime = audio.duration / 2;
-        $(audio).trigger('timeupdate');
-        expect(playerView.$time.val()).toBe('2:15');
+            it("should update value of the time input", function() {
+                var audio = playerView.audio;
+                audio.currentTime = 0;
+                $(audio).trigger('timeupdate');
+                expect(playerView.$time.val()).toBe('0:00');
+                audio.currentTime = audio.duration / 2;
+                $(audio).trigger('timeupdate');
+                expect(playerView.$time.val()).toBe('2:15');
+            });
+
+        });
+
+        it("changing the volumn slider should update the volume", function() {
+            var audio = playerView.audio;
+
+            expect(audio.volume).toBe(1);
+	    playerView.$volumeRange.val(0.5).trigger('change');
+            expect(audio.volume).toBe(0.5);
         });
 
     });
-
-
-  });
 });
